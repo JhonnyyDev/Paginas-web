a73a3f07cea36970115ca6cdc9c17cba80ae1796
JhonnyyDev <Jhonnyydev@gmail.com>
2024-12-30 20:27:49 -0300

    agregue 2 componentes

--- Web1/src/app/barra-navegacion/barra-navegacion.component.ts
+++ Web1/src/app/barra-navegacion/barra-navegacion.component.ts
@@ -1,20 +1,28 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-barra-navegacion',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './barra-navegacion.component.html',
   styleUrl: './barra-navegacion.component.css'
 })
 export class BarraNavegacionComponent {
 
-  modoclaro = false;
+modoclaro = false;
 
-  cambiomodo() {
-   this.modoclaro = !this.modoclaro;
-   if (this.modoclaro) {document.body.classList.add("white");
-   }
-   else {document.body.classList.remove("white");
-   };  
-   }
+cambiomodo() {
+  this.modoclaro = !this.modoclaro;
+  if (this.modoclaro) {document.body.classList.add("white");
+  }
+  else {document.body.classList.remove("white");
+  };  
+}
+
+menuOpen = false;
+
+abrirMenu() {
+  this.menuOpen = !this.menuOpen
+}
+
   }
